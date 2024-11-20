2f3ef1db5ebe22941476c92f947227d32787ba45
Ajinkya D <ajinkyadhotre201@gmail.com>
2024-11-20 19:03:26 +0000

    sorting

--- Sorting.cpp
+++ Sorting.cpp
@@ -12,15 +12,45 @@ void bubbleSort(int arr[], int n) {
         }
     }
 
-   for (int i = 0; i < n - 1; i++){
-    cout<<arr[i] <<", ";
-   }
+   
+}
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j; // Find the index of the smallest element
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
 }
 
 int main() {
     int arr[] = {90,231,12312,1312,2312,323,322,111};
     int len = sizeof (arr) / sizeof(arr[0]);
     // bubbleSort(arr, len);
+    selectionSort(arr,len);
+
+    for (int i = 0; i < len; i++){
+    cout<<arr[i] <<", ";
+   }
+    
     
     return 0;
 }
